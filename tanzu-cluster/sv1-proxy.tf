d63c0a53665b968454a5b259ce8229d733a98342
Rajesh <n_rajeshdit@yahoo.com>
2023-11-21 11:03:43 +0800

    cleanup done

--- tanzu-cluster/sv1-proxy.tf
+++ tanzu-cluster/sv1-proxy.tf
@@ -1,20 +1,20 @@
-module "sv1-terraform-demo" {
+module "sv1-proxy-blue-demo" {
   source            = "../cluster_bootstrap/"
-  cluster_name      = "sv1-terraform-demo"
+  cluster_name      = "sv1-proxy-blue-demo"
   clustergroup = "dev-packages-demo"
   mgt_cluster_name  = "homelab-dev-tanzu"
   provisioner_name  = "sv1-dev-sandbox-vns"
-  tkg_version       = "v1.24.11+vmware.1-fips.1-tkg.1.ubuntu"
-  control_vm_class  = "best-effort-large"
+  tkg_version       = "v1.22.9---vmware.1-tkg.1.cc71bc8"
+  control_vm_class  = "homelab-small-workernode-1"
   worker_count      = "3"
-  work_class        = "best-effort-small"
-  description       = "demo for the vmware demo cluster provision"
+  work_class        = "best-effort-large"
+  description       = "demo for the proxy blue/green deployment"
   high_availability = "false"  
   labels = {
     "app" : "Internal",
     "ci-account" : "homelab",
     "ci-department" : "IT",
-    "ci-e2customer" : "internal",
+    "ci-customer" : "internal",
     "ci-env" : "dev",
   }
 }
